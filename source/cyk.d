fdcd0c8720c6c325d5a539e3a902d6768819066f
Jakob Kogler <jakob.kogler@gmail.com>
2021-01-10 22:24:09 +0100

    simplified data structure

--- source/cyk.d
+++ source/cyk.d
@@ -1,15 +1,17 @@
-import std.array : array, split;
-import std.algorithm : map, filter;
-import std.conv : to;
+import std.array : array, split, byPair, assocArray;
+import std.algorithm : map;
 import std.regex : ctRegex, match, matchAll;
-import std.typecons : Tuple, tuple;
+import std.typecons : Tuple;
 
-struct ProductionRule {
+alias Expansion = string[];
+alias Expansions = Expansion[];
+
+struct ParsedRule {
     string left;
-    string[][] expansions;
+    Expansions expansions;
 }
 
-ProductionRule parseProductionRule(in string rule) {
+ParsedRule parseProductionRule(in string rule) {
     static const re = ctRegex!`^\s*(\w+)\s*→([\w"|\s]+)$`;
     const m = match(rule, re);
     const c = m.captures;
@@ -20,113 +22,121 @@ ProductionRule parseProductionRule(in string rule) {
     }
 
     auto expansions = c[2].split("|").map!parseExpansion.array;
-    return ProductionRule(c[1], expansions);
+    return ParsedRule(c[1], expansions);
 }
 unittest {
-    void check(string rule, ProductionRule expected) {
+    void check(string rule, ParsedRule expected) {
         const parsed = parseProductionRule(rule);
         assert(parsed == expected);
     }
 
-    check(`Foo → A B | C`, ProductionRule("Foo", [["A", "B"], ["C"]]));
-    check(`Bar → "baz" B | C`, ProductionRule("Bar", [[`"baz"`, "B"], ["C"]]));
-    check(`Foo → A | B | C`, ProductionRule("Foo", [["A"], ["B"], ["C"]]));
-    check(`Baz → Foo Bar`, ProductionRule("Baz", [["Foo", "Bar"]]));
-    check(`Baz → "a" "b"`, ProductionRule("Baz", [[`"a"`, `"b"`]]));
-    check(`Bar → "bar"`, ProductionRule("Bar", [[`"bar"`]]));
+    check(`Foo → A B | C`, ParsedRule("Foo", [["A", "B"], ["C"]]));
+    check(`Bar → "baz" B | C`, ParsedRule("Bar", [[`"baz"`, "B"], ["C"]]));
+    check(`Foo → A | B | C`, ParsedRule("Foo", [["A"], ["B"], ["C"]]));
+    check(`Baz → Foo Bar`, ParsedRule("Baz", [["Foo", "Bar"]]));
+    check(`Baz → "a" "b"`, ParsedRule("Baz", [[`"a"`, `"b"`]]));
+    check(`Bar → "bar"`, ParsedRule("Bar", [[`"bar"`]]));
 }
 
-struct UnitProduction {
-    string variable;
-    int left;
-    string right;
+bool isTerminalRule(T)(in T production) {
+    return production.value.length == 1 && production.value[0].length == 1
+        && production.value[0][0].isTerminal;
 }
 
-struct MultiProduction {
-    string variable;
-    int left;
-    int[][] right;
-}
+auto simplify(ParsedRule[] rules) {
+    Expansions[string] productions;
 
-bool isTerminalRule(in ProductionRule rule) {
-    return rule.expansions.length == 1 && rule.expansions[0].length == 1
-        && rule.expansions[0][0][0] == '"';
-}
-
-Tuple!(UnitProduction[], MultiProduction[]) toNumbers(ProductionRule[] rules) {
-    /* return tupleof */
-    UnitProduction[] unitProductions;
-    MultiProduction[] multiProductions;
-    int[string] nonterminals;
-
-    // find indices for nonterminals
     foreach (rule; rules) {
-        if (!(rule.left in nonterminals)) {
-            nonterminals[rule.left] = nonterminals.length.to!int;
-        }
+        productions[rule.left] ~= rule.expansions.dup;
     }
-
-    // now replace tokens with indices, and simultaniously do TERM
-    int numberNonTerminals = nonterminals.length.to!int;
-    foreach (rule; rules) {
-        if (rule.isTerminalRule) {
-            unitProductions ~= UnitProduction(rule.left, nonterminals[rule.left], rule.expansions[0][0][1..$-1]);
-        } else {
-            int[] toIndices(string[] tokens) {
-                int[] indices;
-                foreach (token; tokens) {
-                    if (token[0] == '"') {
-                        unitProductions ~= UnitProduction("", numberNonTerminals, token[1..$-1]);
-                        indices ~= numberNonTerminals;
-                        numberNonTerminals++;
-                    } else {
-                        indices ~= nonterminals[token];
-                    }
-                }
-                return indices;
-            }
-
-            multiProductions ~= MultiProduction(rule.left,
-                                                nonterminals[rule.left],
-                                                rule.expansions.map!toIndices.array);
-        }
-    }
-
-    return tuple(unitProductions, multiProductions);
+    return productions;
 }
 unittest {
     {
-        auto rules = [ProductionRule("Foo", [["A", "B"], ["B", "A"]]),
-                      ProductionRule("A", [[`"a"`]]),
-                      ProductionRule("B", [[`"b"`]])];
-        const expectedUP = [UnitProduction("A", 1, "a"), UnitProduction("B", 2, "b")];
-        const expectedMP = [MultiProduction("Foo", 0, [[1, 2], [2, 1]])];
-        auto result = toNumbers(rules);
-        assert(result[0] == expectedUP);
-        assert(result[1] == expectedMP);
+        auto rules = [ParsedRule("Foo", [["A", "B"], ["B", "A"]]),
+                      ParsedRule("A", [[`"a"`]]),
+                      ParsedRule("B", [[`"b"`]])];
+        auto expected = ["Foo": [["A", "B"], ["B", "A"]],
+                         "A": [[`"a"`]],
+                         "B": [[`"b"`]]];
+        assert(rules.simplify == expected);
+    }
+    {
+        auto rules = [ParsedRule("Foo", [["A", "B"], ["B", "A"]]),
+                      ParsedRule("Foo", [[`"a"`]]),
+                      ParsedRule("B", [[`"b"`]])];
+        auto expected = ["Foo": [["A", "B"], ["B", "A"], [`"a"`]],
+                         "B": [[`"b"`]]];
+        assert(rules.simplify == expected);
+    }
+}
+
+bool isTerminal(in string s) {
+    return s[0] == '"';
+}
+
+auto TERM(Expansions[string] productions) {
+    Expansions[string] newUnitProductions;
+
+    auto TERM_Expansion(Expansion expansion) {
+        Expansion newExpansion;
+        foreach (token; expansion) {
+            if (token.isTerminal) {
+                string nonTerminal = "Unit_" ~ token[1..$-1];
+                newUnitProductions[nonTerminal] = [[token]];
+                newExpansion ~= nonTerminal;
+            } else {
+                newExpansion ~= token;
+            }
+        }
+        return newExpansion;
+    }
+
+    alias KeyValue = Tuple!(string, "key", Expansions, "value");
+    auto TERM_Production(KeyValue production) {
+        if (production.isTerminalRule) {
+            return production;
+        } else {
+            return KeyValue(production.key, production.value.map!TERM_Expansion.array);
+        }
+    }
+
+    auto newProductions = productions.byPair.map!TERM_Production.array;
+    return (newProductions ~ newUnitProductions.byPair.array).assocArray;
+}
+unittest {
+    {
+        auto productions = ["Foo": [["A", "B"], ["B", "A"]],
+                            "A": [[`"a"`]],
+                            "B": [[`"b"`]]
+        ];
+        auto expected = productions.dup;
+        assert(TERM(productions) == expected);
     }
 
     {
-        auto rules = [ProductionRule("Foo", [["A", `"c"`, "B"], ["B", "A"]]),
-                      ProductionRule("A", [[`"a"`]]),
-                      ProductionRule("B", [[`"b"`]])];
-        const expectedUP = [UnitProduction("", 3, "c"), UnitProduction("A", 1, "a"),
-                            UnitProduction("B", 2, "b")];
-        const expectedMP = [MultiProduction("Foo", 0, [[1, 3, 2], [2, 1]])];
-        auto result = toNumbers(rules);
-        assert(result[0] == expectedUP);
-        assert(result[1] == expectedMP);
+        auto productions = ["Foo": [["A", `"c"`, "B"], ["B", "A"]],
+                            "A": [[`"a"`]],
+                            "B": [[`"b"`]]
+        ];
+        auto expected = ["Foo": [["A", "Unit_c", "B"], ["B", "A"]],
+                          "A": [[`"a"`]],
+                          "B": [[`"b"`]],
+                          "Unit_c": [[`"c"`]]
+        ];
+        assert(TERM(productions) == expected);
     }
 
     {
-        auto rules = [ProductionRule("Foo", [[`"*"`, "Bar", `"*"`]]),
-                      ProductionRule("Bar", [[`"a"`], [`"b"`]])];
-        const expectedUP = [UnitProduction("", 2, "*"), UnitProduction("", 3, "*"),
-                            UnitProduction("", 4, "a"), UnitProduction("", 5, "b")];
-        const expectedMP = [MultiProduction("Foo", 0, [[2, 1, 3]]),
-                            MultiProduction("Bar", 1, [[4], [5]])];
-        auto result = toNumbers(rules);
-        assert(result[0] == expectedUP);
-        assert(result[1] == expectedMP);
+        auto productions = ["Foo": [[`"*"`, "Bar", `"*"`]],
+                            "Bar": [[`"a"`], [`"b"`]]
+        ];
+        auto expected = ["Foo": [["Unit_*", "Bar", "Unit_*"]],
+                         "Bar": [["Unit_a"], ["Unit_b"]],
+                         "Unit_*": [[`"*"`]],
+                         "Unit_a": [[`"a"`]],
+                         "Unit_b": [[`"b"`]]
+        ];
+        assert(TERM(productions) == expected);
     }
 }
